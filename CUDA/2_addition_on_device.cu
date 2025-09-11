760e5aa099082b9ce119d43bf65aa43e42f76806
Shrey Panchasara <shrey.panchasara20@gmail.com>
2025-09-11 16:23:20 +0530

    Update 2_addition_on_device.cu

--- CUDA/2_addition_on_device.cu
+++ CUDA/2_addition_on_device.cu
@@ -1,62 +1,24 @@
-%%writefile addtion_on_device.cu
-
-#include<stdio.h>
-__global__ void add(int *a, int *b, int *c) {
-		*c = *a + *b;
-}
-int main(void) {
-		int a, b, c;	            // host copies of a, b, c
-		int *d_a, *d_b, *d_c;	     // device copies of a, b, c
-		int size = sizeof(int);
-
-		// Allocate space for device copies of a, b, c
-		cudaMalloc((void **)&d_a, size);
-		cudaMalloc((void **)&d_b, size);
-		cudaMalloc((void **)&d_c, size);
-
-		// Setup input values
-		a = 2;
-		b = 7;
-		// Copy inputs to device
-		cudaMemcpy(d_a, &a, size, cudaMemcpyHostToDevice);
-		cudaMemcpy(d_b, &b, size, cudaMemcpyHostToDevice);
-
-		// Launch add() kernel on GPU
-		add<<<1,1>>>(d_a, d_b, d_c);
-
-		// Copy result back to host
-		cudaMemcpy(&c, d_c, size, cudaMemcpyDeviceToHost);
-
-		// Cleanup
-		cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
-    printf("Value of c after device addtion is %d",c);
-		return 0;
-	}
--------------------------------------------------------------
-!nvcc -arch=sm_75 addtion_on_device.cu -o aod
-!./aod
--------------------------------------------------------------
 %%writefile P_AOD.cu
 
 #include<stdio.h>
 #define N 512
 __global__ void add(int *a, int *b, int *c) {
-		c[blockIdx.x] = a[blockIdx.x] + b[blockIdx.x];
+                c[blockIdx.x] = a[blockIdx.x] + b[blockIdx.x];
 }
 int main(void) {
-	int *a, *b, *c;		// host copies of a, b, c
-	int *d_a, *d_b, *d_c;	// device copies of a, b, c
-	int size = N * sizeof(int);
+        int *a, *b, *c;         // host copies of a, b, c
+        int *d_a, *d_b, *d_c;   // device copies of a, b, c
+        int size = N * sizeof(int);
 
-	// Alloc space for device copies of a, b, c
-	cudaMalloc((void **)&d_a, size);
-	cudaMalloc((void **)&d_b, size);
-	cudaMalloc((void **)&d_c, size);
+        // Alloc space for device copies of a, b, c
+        cudaMalloc((void **)&d_a, size);
+        cudaMalloc((void **)&d_b, size);
+        cudaMalloc((void **)&d_c, size);
 
-	// Alloc space for host copies of a, b, c and setup input values
-	a = (int *)malloc(size);
-	b = (int *)malloc(size);
-	c = (int *)malloc(size);
+        // Alloc space for host copies of a, b, c and setup input values
+        a = (int *)malloc(size);
+        b = (int *)malloc(size);
+        c = (int *)malloc(size);
   int counter;
   for(counter = 0; counter < N; counter++){
     a[counter] = counter;
@@ -67,7 +29,7 @@ int main(void) {
         cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
 
         // Launch add() kernel on GPU with N blocks
-        add<<>>(d_a, d_b, d_c);
+        add<<<N,1>>>(d_a, d_b, d_c);
 
         // Copy result back to host
         cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
@@ -81,6 +43,6 @@ int main(void) {
         }
         return 0;
   }
-----------------------------------------------------------------
+-------------------------------------------------------------------------------------
 !nvcc -arch=sm_75 P_AOD.cu -o p_aod
-!./p_aod
+%time !./p_aod
